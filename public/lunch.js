c3aea0eeb5089100853c5af562b22c0393d0b48e
thisisnew <thisisnew@naver.com>
2021-10-07 23:57:35 +0900

    점심추천 api 생성

--- public/lunch.js
+++ public/lunch.js
@@ -14,4 +14,28 @@ window.onload = function() {
             }
         }
     });
+
+    const recommend = document.querySelector(".recommend");
+    recommend.addEventListener("click", () => {
+        let item;
+        const array = [];
+        for(item of checks){
+            if (item.checked) {
+                const obj = {};
+                const td = item.parentElement;
+                const menu = td.nextElementSibling;
+                const place = menu.nextElementSibling;
+                obj["menu"] = menu.innerHTML;
+                obj["place"] = place.innerHTML;
+                array.push(obj);
+            }
+        }
+
+        if(Array.isArray(array) && !array.length) {
+            alert("1개 이상의 메뉴를 선택해주세요.");
+            return;
+        }
+
+
+    });
 };
