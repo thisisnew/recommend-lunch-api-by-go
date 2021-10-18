fa80adbfec8708ea967abe4c637b4e02c5d8cdcc
thisisnew <thisisnew@naver.com>
2021-10-18 22:54:48 +0900

    점심추천 api 생성

--- public/js/const.js
+++ public/js/const.js
@@ -1,3 +1,4 @@
 const LOCAL_DATA_072404_GA = 'LOCALDATA_072404_GA';
 const INFO_000 = 'INFO-000';
 const CLOSED = '폐업';
+const GWANAK_RESTAURANT_LIST_URL = 'http://openapi.seoul.go.kr:8088/sample/json/LOCALDATA_072404_GA/1/5/';
